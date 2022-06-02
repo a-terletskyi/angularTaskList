a073d16b5df099816b920bcebb74dbc606c82ef3
aterletsky123 <terletsky28@gmail.com>
2022-06-02 22:35:31 +0300

    added logic for table

--- src/app/task-list/table/table.component.ts
+++ src/app/task-list/table/table.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -7,8 +6,31 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 
 export class TableComponent implements OnInit {
+  @Input() tasks!: { name: string; check: boolean; }[];
+  tableIptValue!: string;
+  taskIndex!: number;
+  isSave = false;
 
   constructor() { }
   ngOnInit(): void { }
 
+  changeCheckBox(index: number): void { this.tasks[index].check = !this.tasks[index].check }
+
+  editTask(index: number): void {
+    this.tableIptValue = this.tasks[index].name;
+    this.taskIndex = index;
+    this.isSave = true;
+  }
+
+  deleteTask(index: number): void {
+    this.tasks.splice(index, 1);
+    if (this.isSave) this.isSave = false;
+  }
+
+  saveTask(input: HTMLInputElement): void {
+    if (this.tableIptValue === '') { input.placeholder = 'Please write text...' } else {
+      this.tasks[this.taskIndex].name = this.tableIptValue;
+      this.isSave = false;
+    }
+  }
 }
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
+
 export class TaskListComponent implements OnInit {
+  inputValue = '';
   user = { firstName: 'Ivan', lastName: 'Ivanov' };
-  userTasks: Array<object> = [
-    { task: 'HTML', check: true, status: 'Done' },
-    { task: 'CSS3', check: true, status: 'Done' },
-    { task: 'SCSS', check: false, status: 'In PROGRESS' }
+  userTasks: { name: string; check: boolean; }[] = [
+    { name: 'HTML', check: true },
+    { name: 'CSS3', check: true },
+    { name: 'SCSS', check: false },
+    { name: 'JavaScript', check: false },
+    { name: 'JQuery', check: false },
+    { name: 'Angular', check: false },
+    { name: 'Bootstrap', check: false },
   ];
 
   constructor() { }
   ngOnInit(): void { }
+
+  addTask(input: HTMLInputElement): void {
+    if (this.inputValue === '') { input.placeholder = 'Please write text...' } else {
+      this.userTasks.push({ name: this.inputValue, check: false });
+      this.inputValue = '';
+      input.placeholder = '';
+    }
+  }
+
 }
+
+// Plan
+// Добавити паралельно клас для Done - green, In progress - red
+// Застилізувати кнопки в таблиці
